5accac47cf0c31f4519bf2777413ec31e86f15a6
baitieyi <baitieyi@163.com>
2018-03-08 21:50:45 +0800

    hash

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ var http = require('http')
 var crypto = require('crypto')
 var config = require('./config.json')
 
-function getHash (str) {
-  var hmac = crypto.createHmac('sha1', config.secret)
+function getHash (alg, str) {
+  var hmac = crypto.createHmac(alg, config.secret)
   hmac.update(str)
-  return hmac.digest('hex')
+  return hmac.digest('hex').toString()
 }
 
 function exec () {
@@ -28,9 +28,14 @@ var server = http.createServer((req, res) => {
     console.log(req.headers['x-github-event'])
     if (!~config.event.indexOf(req.headers['x-github-event'])) return
     console.log('to get hash')
-    var hash = getHash(data.toString())
+    var [alg, githash] = req.headers['x-hub-signature'].split('=')
+    var hash = getHash(alg, data)
     console.log(hash)
+    if (hash !== githash) return
     exec()
+  })
+  req.on('end', () => {
+    console.log('end')
   })
   res.end('get')
 })
