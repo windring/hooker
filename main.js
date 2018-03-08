2bf805bc2cdc62455f4a1edf7357ede349bdd21e
baitieyi <baitieyi@163.com>
2018-03-08 19:35:11 +0800

    test

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 var http = require('http')
 var server = http.createServer((req, res) => {
   console.log(req.headers)
+  console.log(req.url)
+  if (req.url) {
+    var exec = require('child_process')
+    exec.execFile('./sh/main.sh', (error, stdout, stderr) => {
+      if (error) {
+        throw error
+      }
+      console.log(stdout)
+    })
+  }
   res.end()
 })
 server.listen(1141, '0.0.0.0')
