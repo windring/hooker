92f2955bdb18ee233b59f0ff05676e0dea70d2cb
baitieyi <baitieyi@163.com>
2018-03-08 21:17:51 +0800

    hash

--- main.js
+++ main.js
@@ -2,14 +2,23 @@ var http = require('http')
 var crypto = require('crypto')
 var config = require('./config.json')
 
-console.log(crypto.createHash('sha1').update('12138').digest('hex'))
+function getHash (str) {
+  var hmac = crypto.createHmac('sha1', config.secret)
+  hmac.update()
+  return hmac.digest('hex')
+}
 
 var server = http.createServer((req, res) => {
-  console.log(req.headers)
-  console.log(req.url)
-  if (req.headers['x-github-event']) {
+  req.on('data', (data) => {
+    console.log(data)
+    if (!req.headers['x-github-event']) return
     console.log(req.headers['x-github-event'])
-  }
+    if (!~config.event.indexOf(req.headers['x-github-event'])) return
+    console.log('get hash')
+    var hash = getHash(data.toString())
+    console.log(hash)
+  })
+  /*
   if (req.url) {
     var exec = require('child_process')
     exec.execFile('./sh/main.sh', (error, stdout, stderr) => {
@@ -20,6 +29,7 @@ var server = http.createServer((req, res) => {
       console.log(stdout)
     })
   }
+  */
   res.end('get')
 })
 server.listen(config.port, () => {
